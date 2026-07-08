424b93e65afa6719ccdeddea2cdca8887c334dd7
wwwchangjin <changjinwang1226@tongji.edu.cn>
2026-07-08 16:17:20 +0800

    finish find

---
+++ user/find.c
@@ -0,0 +1,87 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+void
+find(char *path, char *target)
+{
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if((fd = open(path, 0)) < 0){
+    fprintf(2, "find: cannot open %s\n", path);
+    return;
+  }
+
+  if(fstat(fd, &st) < 0){
+    fprintf(2, "find: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  if(st.type == T_FILE){
+    char *name;
+
+    for(name = path + strlen(path); name >= path && *name != '/'; name--)
+      ;
+    name++;
+
+    if(strcmp(name, target) == 0)
+      printf("%s\n", path);
+
+    close(fd);
+    return;
+  }
+
+  if(st.type == T_DIR){
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof(buf)){
+      fprintf(2, "find: path too long\n");
+      close(fd);
+      return;
+    }
+
+    strcpy(buf, path);
+    p = buf + strlen(buf);
+    *p++ = '/';
+
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+      if(de.inum == 0)
+        continue;
+
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+
+      if(strcmp(p, ".") == 0 || strcmp(p, "..") == 0)
+        continue;
+
+      if(stat(buf, &st) < 0){
+        fprintf(2, "find: cannot stat %s\n", buf);
+        continue;
+      }
+
+      if(strcmp(p, target) == 0)
+        printf("%s\n", buf);
+
+      if(st.type == T_DIR)
+        find(buf, target);
+    }
+  }
+
+  close(fd);
+}
+
+int
+main(int argc, char *argv[])
+{
+  if(argc != 3){
+    fprintf(2, "usage: find path filename\n");
+    exit(1);
+  }
+
+  find(argv[1], argv[2]);
+
+  exit(0);
+}
