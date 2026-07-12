055273c151f385add66896a235f4852c9f9c81fe
wwwchangjin <changjinwang1226@tongji.edu.cn>
2026-07-12 17:01:25 +0800

    finish pthread hash table

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,8 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t bucket_lock[NBUCKET];
+
 
 double
 now()
@@ -36,25 +38,26 @@ insert(int key, int value, struct entry **p, struct entry *n)
   *p = e;
 }
 
-static 
-void put(int key, int value)
+static void
+put(int key, int value)
 {
   int i = key % NBUCKET;
 
-  // is the key already present?
+  pthread_mutex_lock(&bucket_lock[i]);
+
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
   }
-  if(e){
-    // update the existing key.
+
+  if (e) {
     e->value = value;
   } else {
-    // the new is new.
     insert(key, value, &table[i], table[i]);
   }
 
+  pthread_mutex_unlock(&bucket_lock[i]);
 }
 
 static struct entry*
@@ -62,11 +65,15 @@ get(int key)
 {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&bucket_lock[i]);
 
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
-    if (e->key == key) break;
+    if (e->key == key)
+      break;
   }
+
+  pthread_mutex_unlock(&bucket_lock[i]);
 
   return e;
 }
@@ -112,6 +119,10 @@ main(int argc, char *argv[])
   }
   nthread = atoi(argv[1]);
   tha = malloc(sizeof(pthread_t) * nthread);
+
+  for (int i = 0; i < NBUCKET; i++) {
+  assert(pthread_mutex_init(&bucket_lock[i], NULL) == 0);
+  }
   srandom(0);
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
